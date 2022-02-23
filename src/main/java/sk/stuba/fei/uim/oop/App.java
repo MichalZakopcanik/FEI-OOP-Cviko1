23df2b60aee7109438838b7960c2d9389aebd2b3
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-02-23 15:02:55 +0100

    spajanie "roznych" typov

--- src/main/java/sk/stuba/fei/uim/oop/App.java
+++ src/main/java/sk/stuba/fei/uim/oop/App.java
@@ -8,7 +8,6 @@ public class App {
         int i = 345;
         int j = 10;
         String s = "abc";
-
         System.out.println("int i = " + i + " int j = " + j + " = " + (i + j));
         System.out.println("String s = " + s + " boolean b " + b + " = " +(s+b));
         System.out.println("int i = " + i + " boolean b " + b + " = " + i+b);
